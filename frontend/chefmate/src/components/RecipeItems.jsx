1f83c2b9728dd71e42612bcb66cd60c09454a9b4
Kenji-x-S <rllynwatme3@gmail.com>
2025-06-16 03:23:17 +0500

    Edit button redirect added

--- frontend/chefmate/src/components/RecipeItems.jsx
+++ frontend/chefmate/src/components/RecipeItems.jsx
@@ -1,11 +1,14 @@
 import React from 'react'
-import { useLoaderData } from 'react-router-dom'
+import { Link, useLoaderData } from 'react-router-dom'
 import burgerimg from '../assets/food.jpeg'
 import {BsStopwatchFill} from "react-icons/bs";
 import {FaHeart} from "react-icons/fa6";
+import {FaEdit} from "react-icons/fa";
+import {MdDelete} from "react-icons/md";
 
 export default function RecipeItems() {
     const allRecipes=useLoaderData()
+    let path=window.location.pathname==="/myRecipe" ? true:false
     console.log(allRecipes)
     return(
         <div className='card-container'>
@@ -18,7 +21,11 @@ export default function RecipeItems() {
                                 <div className='title'>{item.title}</div>
                                 <div className='icons'>
                                     <div className='timer'><BsStopwatchFill/></div>
-                                    <FaHeart/>
+                                    {(!path)?<FaHeart/>:
+                                    <div className='action'>
+                                        <Link to={`/editRecipe/${item._id}`} className="editIcon"><FaEdit/></Link>
+                                        <MdDelete className='deleteIcon'/>
+                                    </div>}
                                 </div>
                             </div>
                         </div>
